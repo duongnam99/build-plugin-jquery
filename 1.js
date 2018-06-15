ff5280973fb226e299c0a628a1249a4292ee2827
duongnam99 <duongminhnam99@gmail.com>
2018-06-16 00:16:58 +0700

    up

---
+++ 1.js
@@ -0,0 +1,87 @@
+﻿$(function( $ ) { 
+$.fn.inner_float = function(options) { // <=> window.jQuery.prototype.inner_float = function(){options};
+      var defaults = {
+        top: "10px" 
+      }
+      
+      // overwritten/Merge
+      var set = $.extend(true, defaults, options);
+      console.log(set);
+      // remove text: "px" (replace by a space !!!)
+      var distance_child_to_top = set.top.replace('px','');
+
+      //convert string to number
+      distance_child_to_top = Number(distance_child_to_top);
+
+      // var state = 'under';
+      var child_offset_top = $(this).offset().top;
+      // console.log($(this));
+      var parent_offset_top = $(this).parent().offset().top;
+
+      var child_width = $(this).width(); // css fix bug: "child" change width when moving     
+
+      var parent_height = $(this).parent().height();
+
+      var child_height = $(this).height();
+      // GET A POINT THAT BOTTOM OF CHILD AND PARENT MEET EACH OTHER
+      var out = parent_offset_top + parent_height - child_height - distance_child_to_top;
+      var child_frozen = parent_height - child_height ; // top of child to top of parent , parent position: relative
+      console.log(child_frozen);
+      // ASSIGN "this" to "child" variable and put it in window scroll function
+      var child = $(this);
+      $(window).scroll(function(event) {
+
+        var window_position = $(window).scrollTop() 
+        // check to take "child" back to default position
+        if(distance_child_to_top <= parent_offset_top){
+            if(window_position < (child_offset_top - distance_child_to_top)){
+               child.css({
+                    'position': '',
+                    'top': '',
+                    'width': String(child_width)+'px'
+                });
+
+            }else if(window_position >= (child_offset_top - distance_child_to_top) && window_position < out){
+               child.css({
+                    'position': 'fixed',
+                    'top': set.top,
+                    'width': String(child_width)+'px'
+                });
+            }
+        }else if(distance_child_to_top >= parent_offset_top){
+          if(window_position == 0){
+              child.css({
+                'position': '',
+                'top': '',
+                'width': String(child_width)+'px'
+               });
+          }else if(window_position > 0 && window_position < out){
+             child.css({
+                  'position': 'fixed',
+                  'top': set.top,
+                  'width': String(child_width)+'px'
+              });
+           }
+        } 
+
+        if(window_position >= out){
+            child.parent().css('position','relative');
+            child.css({
+                'position': 'absolute',
+                'top': String(child_frozen) +'px',  
+                'width': String(child_width)+'px'
+            });
+        }
+
+      });
+    }
+}( jQuery ));
+
+$(function() { 
+    
+    
+    //call 
+    $('#almost-show').inner_float({
+    	top: "10px"
+	});
+});
---
+++ README.md
@@ -0,0 +1,28 @@
+# Bài tập Trainee Colombo 2018  
+## Build Jquery plugin
+- Một số chức năng yêu cầu:
+    - Khi scroll, phần tử con trượt trong phần tử cha và không bị trượt đi mất 
+    - Đến hết chiều dài phần tử cha, phần tử con biến mất cùng phần tử cha
+    - [Chi tiết yêu cầu ](https://github.com/colombo-trainee/trainee_2018/blob/master/frontend/jquery_plugin.md)
+- Thực hiện bởi: **Dương Minh Nam**  
+
+## Liên kết
+- [Demo- jsfiddle](https://jsfiddle.net/mogp9tjf/10/)
+
+## Hướng dẫn sử dụng    
+- Select element (phần tử con muốn có hiệu ứng), gọi hàm inner_float(), truyền object vào hàm  
+vd: khoảng cách div con với lề trên của sổ trình duyệt là 10px
+> $('.className').inner_float({
+>    'top': '10px'
+> })
+## Kiến thức nắm được  
+- khai báo hàm với prototype để xây dựng plugin
+- ôn lại object trong js, hàm viết đè object này lên object khác: extend()
+- sự kiện scroll, các hàm lấy vị trí, lấy chiều dài, rộng của phần tử
+- hàm ép kiểu: String(), Number()...
+
+## Phát triển thêm
+- thêm hiệu ứng khi scroll ngược lên và trở về vị trí ban đầu
+
+## Credit
+- phầm mềm hỗ trợ: thư viện jquery bản 3.3.1
